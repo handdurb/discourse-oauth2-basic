ed3ee3779f01e36ccd61d5fbc5780277444cb797
jessefeng <jessefeng.li@renogy.com>
2025-04-28 15:31:19 +0800

    注册钉钉认证器 dingtalk_authenticator.rb

--- plugin.rb
+++ plugin.rb
@@ -34,4 +34,4 @@ auth_provider title_setting: "oauth2_button_title", authenticator: OAuth2BasicAu
 
 require_relative "lib/validators/oauth2_basic/oauth2_fetch_user_details_validator"
 
-auth_ding_provider authenticator: DingtalkAuthenticator.new
+auth_provider authenticator: DingtalkAuthenticator.new
